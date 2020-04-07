90f48e8120b03ef565a33b378503907aa41fb548
nicklada <61970788+nicklada@users.noreply.github.com>
2020-04-08 01:23:53 +0300

    Limit tests added

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -53,4 +53,30 @@ class BonusServiceTest {
         //производим проверку(сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+    
+     @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndLimit() {
+        BonusService service = new BonusService();
+        //подготавливаем данные:
+        long amount = 1_666_667;
+        boolean registered = false;
+        long expected = 500;
+        //вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        //производим проверку(сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+         
+    }
+      @org.junit.jupiter.api.Test
+    void shouldCalculateForNotRegisteredAndLimit() {
+        BonusService service = new BonusService();
+        //подготавливаем данные:
+        long amount = 5_000_000;
+        boolean registered = false;
+        long expected = 500;
+        //вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        //производим проверку(сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 }
